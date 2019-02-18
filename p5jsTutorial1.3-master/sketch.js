37e73c8c51e9f1a7f4f6ac993339adba8aa3d086
PS20221033 <46880568+PS20221033@users.noreply.github.com>
2019-02-18 12:48:41 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,37 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(255,5,255)
+
+	fill(0,255,255);
+	stroke(0,0,0);
+	strokeWeight(3);
+
+
+	rect(10,10,100,50);
+
+  rect(201,250,500,50)
+
+
+	rect(10,100,100,50);
+
+	fill(0,0,255);
+  stroke(0,0,0);
+  strokeWeight(0);
+
+  fill(0,255,255)
+
+	text("Donald Trump",11,130);
+
+  fill(250,250,250)
+
+	text("ooffed ooofed",25,130);
+
+  text("Your Name",30,40);
+
+}
